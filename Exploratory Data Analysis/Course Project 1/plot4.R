003f43114ec44fd536c7b5a26b7befe94b2ada46
indiandataboy <indiandataboy@protonmail.com>
2020-05-06 23:20:38 +0530

    Space error in plot

--- Exploratory Data Analysis/Course Project 1/plot4.R
+++ Exploratory Data Analysis/Course Project 1/plot4.R
@@ -11,7 +11,7 @@ plot(req_data$Time, req_data$Global_active_power, type = "l", ylab = "Global Act
 plot(req_data$Time, req_data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 with(req_data, {lines(Time, Sub_metering_2, col = "red")
     lines(Time, Sub_metering_3, col = "blue")})
-legend("topright", legend = c("Sub_metering_1", "Sub_meteri ng_2", "Sub_metering_3"), col = c("black", "red", "blue"), lwd = 1)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lwd = 1)
 
 plot(req_data$Time, req_data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
 
